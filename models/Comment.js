288d9a5e524dd997d0b4b3846494dcace8eaf3ff
Josh Taylor <josh@joshtaylordesign.com>
2021-05-03 14:05:44 -0700

    added comment model

---
+++ models/Comment.js
@@ -0,0 +1,23 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt =require("bcrypt");
+
+class Comment extends Model {}
+
+User.init({
+    content:{
+        type:DataTypes.STRING(255),
+    },
+    user_id:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    }
+    card_id:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    }
+},{
+    sequelize
+})
+
+module.exports = Comment

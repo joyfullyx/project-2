3b80cd5951b524edf74a9f75ddf52f0b2cfec59f
Gushihiro <robinsonhiroto@gmail.com>
2021-05-05 15:34:25 -0700

    got comment functionality working, comment routes properly CRUD, front end form connected with comments api and comments nested into the pertaining cards

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,16 +1,31 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
+const router = require('express').Router({mergeParams: true});
+const { Comment, Card } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.get('/', withAuth, async (req, res) => {
+    try{
+        const commentData = await Comment.findAll({
+            include: [{ model: Card }],
+        });
+        res.status(200).json(commentData)
+        console.log(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err);
+    }
+})
+
+router.post('/:id', withAuth, async (req, res) => {
+    console.log("THIS IS req.params", req.params);
     try {
         const newComment = await Comment.create({
-            ...req.body,
-            user_id: req.session.user_id
+            content: req.body.content,
+            user_id: req.session.user_id,
+            card_id: req.params.id,
         });
-
         res.status(200).json(newComment);
     } catch (err) {
+        console.log(err);
         res.status(400).json(err);
     }
 });

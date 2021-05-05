1294ce72a1c7f372d1f919e7cc4ce206bffbc87c
Gushihiro <robinsonhiroto@gmail.com>
2021-05-05 11:27:06 -0700

    circle testing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const cardRoutes = require('./cardRoutes');
 const categoryRoutes = require('./categoryRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 router.use('/categories', categoryRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;

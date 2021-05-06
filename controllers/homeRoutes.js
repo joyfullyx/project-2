8d2ab1ebbc83f6108e1f33ec41472df788162d66
Joy Rhee <joyfullyx@gmail.com>
2021-05-06 11:37:30 -0700

    cards showing on logged in profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
     var ip = '71.231.34.183';
     
     // TEST IP ADDRESS 
-    var ip = "207.97.227.239";
+    // var ip = "207.97.227.239";
     console.log('ip:', ip);
     // console.log('req.ip:', req.ip);
     var geo = geoip.lookup(ip);
@@ -42,9 +42,8 @@ router.get("/", async (req, res) => {
           model: Comment
         }
       ]
-    },
-  }
-  });
+    // },
+  })
     // // Serialize data so the template can read it
     // const card = cardData.map((card) => card.get({ plain: true }));
 
@@ -71,16 +70,16 @@ router.get("/", async (req, res) => {
     // console.log('cardData: ', cardData)
 
 //     // Pass serialized data and session flag into template
-//     res.render("homepage", { card: cards });
-//   } catch (err) {
-//     res.status(500).json(err);
-//     console.log(err);
-//   }
-// });
+    res.render("homepage", { card: cards });
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
 
 // ==============================================
 
-router.get('/', async (req, res) => {
+// router.get('/', async (req, res) => {
   // try {
   //   const cardData = await Card.findAll( {
   //     include: [
@@ -97,8 +96,8 @@ router.get('/', async (req, res) => {
   //   res.status(500).json(err);
   //   console.log(err);
   // }
-  res.render('homepage');
-});
+  // res.render('homepage');
+// });
 
 router.get('/cards/:id', async (req, res) => {
   try {
@@ -139,32 +138,102 @@ router.get('/categories', async (req, res) => {
   }
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  // console.log(req.session);
-  // res.send(`welcome, ${req.session.user_id}`);
-  
+router.get('/profile', withAuth, async(req, res) => {
+
   try {
-    // Find the logged in user based on the session ID
+    var forwardedIpsStr = req.header('x-forwarded-for');
+    // JOY'S IP ADDRESS
+    var ip = '71.231.34.183';
+    // TEST IP ADDRESS 
+    // var ip = "207.97.227.239";
+    console.log('ip:', ip);
+
+    var geo = geoip.lookup(ip);
+    var lat = parseFloat(geo.ll[0]);
+    var lon = parseFloat(geo.ll[1]);
+    var city = geo.city;
+    var state = geo.region;
+    console.log('city, state: ', city, state, lat, lon);
+    console.log('The IP is %s', geoip.pretty(ip));
+
+
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { 
-        exclude: ['password'] 
+      attributes: {
+        exclude: ['password']
       },
-    });
+    })
 
     const user = await userData.get({ plain: true });
-    // const card = cardData.get({ plain: true });
+    const cardData = await Card.findAll(req.params.id, {
+      include: [
+        {
+          model: User,
+          model: Comment
+        }
+      ]
+    })
 
-    // res.render('profile', {card});
-    res.render('profile', {
-      ...user, 
-      logged_in: true
+    const cards = cardData.filter((card) => {
+      console.log('distance between ip and event in miles: ', (getDistanceLatLonToMiles(
+        lat, 
+        lon,
+        parseFloat(card.event_location_lat),
+        parseFloat(card.event_location_lon),
+      )))
+      if (
+        getDistanceLatLonToMiles(
+          lat,
+          lon,
+          parseFloat(card.event_location_lat),
+          parseFloat(card.event_location_lon)
+        ) < 5570.00
+      )
+      return card.get({ plain: true });
     });
+
+    console.log("cards: ", cards);
+    res.render('profile', {
+      card: cards,
+      ...user,
+      logged_in: true
+    })
   } catch (err) {
     res.status(500).json(err);
     console.log(err);
   }
 });
+
+// ===========trying to get cards to render on profile==========
+
+// =================keep this =====================
+// Use withAuth middleware to prevent access to route
+// router.get('/profile', withAuth, async (req, res) => {
+//   // console.log(req.session);
+//   // res.send(`welcome, ${req.session.user_id}`);
+  
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { 
+//         exclude: ['password'] 
+//       },
+//     });
+
+//     const user = await userData.get({ plain: true });
+//     // const card = cardData.get({ plain: true });
+
+//     // res.render('profile', {card});
+//     res.render('profile', {
+//       ...user, 
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//     console.log(err);
+//   }
+// });
+
+// ============== ^ keep this ^ ===============================
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route

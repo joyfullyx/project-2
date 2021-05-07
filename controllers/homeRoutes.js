dd43b8d03bff3069f37d53d4ed6b402a668820c9
Josh Taylor <josh@joshtaylordesign.com>
2021-05-07 14:11:31 -0700

    custom helper

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -329,7 +329,8 @@ router.get('/profile', withAuth, async(req, res) => {
     res.render('profile', {
       card: allCards,
       ...user,
-      logged_in: true
+      logged_in: true, 
+      currUser: req.session.user_id //test line
     })
   } catch (err) {
     res.status(500).json(err);
@@ -359,8 +360,8 @@ router.get('/signup', (req, res) => {
 router.get('/logout', async (req, res) => {
   if (req.session.logged_in) {
       req.session.destroy(() => {
-          res.status(204).end();
-
+          // res.status(204).end();
+          res.status(204).redirect('/').end();
       });
     if (!req.session) {
       res.redirect('/login');
